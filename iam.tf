ba2c07f17a82d3711ad3f1f7d06976673fe23cda
safwan tahmid <safwantahmid1306@gmail.com>
2026-05-05 02:12:55 +0600

    Add IAM role and policy for RDS monitoring

--- iam.tf
+++ iam.tf
@@ -113,3 +113,22 @@ resource "aws_iam_role_policy" "cloudtrail_policy" {
     ]
   })
 }
+resource "aws_iam_role" "rds_monitoring_role" {
+  name = "${var.environment}-rds-monitoring-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "monitoring.rds.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rds_monitoring_attach" {
+  role       = aws_iam_role.rds_monitoring_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
